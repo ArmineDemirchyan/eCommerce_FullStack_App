6bd331c2ca243d332e23ec94af99840a63dbd533
ArmineDemirchyan <100238894+ArmineDemirchyan@users.noreply.github.com>
2022-11-13 12:29:32 +0400

    register-login-page-design done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
-
+import ProductList from "./pages/ProductList";
+import Cart from "./pages/Cart";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 const App = () => {
   return (
-    <div>
-      <Product/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/products" element={<ProductList/>}/>
+      <Route path="/product" element={<Product/>}/>
+      <Route path="/cart" element={<Cart/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 };
 
