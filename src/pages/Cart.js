53387ecc1ea03533ff2b0e6a72da455fd2ada8d9
ArmineDemirchyan <100238894+ArmineDemirchyan@users.noreply.github.com>
2022-11-28 22:19:45 +0400

    final commit

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -175,7 +175,7 @@ const Cart = () => {
         <Bottom>
           <Info>
             {cart.products?.map((product) => (
-              <Product>
+              <Product key={product._id}>
                 <ProductDetail>
                   <Image src={product.img} />
                   <Details>
